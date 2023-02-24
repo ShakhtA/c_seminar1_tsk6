18f004efe897205887efde91a07dc6e90faed19a
Andrey <shakht_a@mail.ru>
2023-02-24 11:28:07 +0300

    задача 6 проверка числа на четность

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Ввод числа
+
+Console.WriteLine("Введите число");
+
+string Str = Console.ReadLine() ?? "";
+int Num = int.Parse(Str);
+
+// Проверка числа на четность
+
+if (Num % 2 == 0) Console.WriteLine($"{Num} - четное число");
+else Console.WriteLine($"{Num} - нечетное число");    
